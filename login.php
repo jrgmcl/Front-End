948fd45d7e1de1c7e24d63769fa7317078530ab9
jrgmcl <83060373+jrgmcl@users.noreply.github.com>
2022-03-20 01:58:56 +0800

    Renamed CSS file
    All PHP and CSS files are now stored on the same folder (Some CSS scripts renamed)

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>FaceCognition</title>
-	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link rel="stylesheet" type="text/css" href="login-reg-styles.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
