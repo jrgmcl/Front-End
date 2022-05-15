7c857540871ebeb22eb763c3f84ed561a2de00ad
jrgmcl <jorgegalang21@gmail.com>
2022-05-16 01:30:42 +0800

    Restored

--- config.php
+++ config.php
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-<?php
-#import that latest database file from https://github.com/jrgmcl/Front-End
-$host ="localhost";
-$username = "root";
-$password = "";
-$dbname = "fr";
-
-$conn = new mysqli($host, $username, $password, $dbname);
-if($conn->connect_error){
-	die("Connection Failed!");
-
-}
-=======
 <?php
 #import that latest database file from https://github.com/jrgmcl/Front-End
 $host ="localhost";
@@ -25,4 +11,3 @@ if($conn->connect_error){
 
 }
 ?>
->>>>>>> 5f5b14b61e94c55a9a23ed64613097463a6bfb08
