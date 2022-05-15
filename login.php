0b5c447f8cd4a93408d0ce9dc25b2a10aad4c9bd
jrgmcl <jorgegalang21@gmail.com>
2022-05-15 12:31:04 +0800

    change linking

--- login.php
+++ login.php
@@ -11,7 +11,7 @@ include 'err.php';
 
 <head>
 	<title>FaceCognition</title>
-	<link rel="stylesheet" type="text/css" href="login-reg-styles.css">
+	<link rel="stylesheet" type="text/css" href="css/login-reg-styles.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
