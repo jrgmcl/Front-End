48da4f047a676aea7a05ba67b2ebc5a521fd05cf
adeldiola <adelainediola@gmail.com>
2022-05-21 16:02:47 +0300

    Update admin_page.php
    header > index.php

--- admin_page.php
+++ admin_page.php
@@ -4,14 +4,15 @@
 
 session_start();
 
-if(!isset($_SESSION['admin_name'])){
-   header('location:Login.php');
+if (!isset($_SESSION['admin_name'])) {
+   header('location:index.php');
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,20 +23,22 @@ if(!isset($_SESSION['admin_name'])){
    <link rel="stylesheet" href="style.css">
 
 </head>
-<body>
-   
-<div class="container">
 
-   <div class="content">
-      <h3>hi, <span>admin</span></h3>
-      <h1>welcome <span><?php echo $_SESSION['admin_name'] ?></span></h1>
-      <p>this is an admin page</p>
-      <a href="Login.php" class="btn">login</a>
-      <a href="Register.php" class="btn">register</a>
-      <a href="Logout.php" class="btn">logout</a>
+<body>
+
+   <div class="container">
+
+      <div class="content">
+         <h3>hi, <span>admin</span></h3>
+         <h1>welcome <span><?php echo $_SESSION['admin_name'] ?></span></h1>
+         <p>this is an admin page</p>
+         <a href="Login.php" class="btn">login</a>
+         <a href="Register.php" class="btn">register</a>
+         <a href="Logout.php" class="btn">logout</a>
+      </div>
+
    </div>
 
-</div>
-
 </body>
+
 </html>
