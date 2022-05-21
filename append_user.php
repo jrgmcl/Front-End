a26a6b0bfcda9bf7ed696029f6699e7b03ee6890
adeldiola <adelainediola@gmail.com>
2022-05-21 16:05:50 +0300

    Renamed window location > index.php

--- append_user.php
+++ append_user.php
@@ -6,14 +6,14 @@ $ru_lastname = $_GET["ru_lastname"];
 $ru_studentid = $_GET["ru_studentid"];
 $ru_course = $_GET["ru_course"];
 $ru_email = $_GET["ru_email"];
-$newfilename = $ru_firstname.".".$ru_lastname;
+$newfilename = $ru_firstname . "." . $ru_lastname;
 
 $dataset = "/var/www/html/datasets/";
 $file_name = $_FILES['upload']['name'];
 $temp_path = $_FILES['upload']['tmp_name'];
-$destination_path = $temp_path.$file_name;
+$destination_path = $temp_path . $file_name;
 $total = count($file_name);
-$imageFileType = strtolower(pathinfo($destination_path,PATHINFO_EXTENSION));
+$imageFileType = strtolower(pathinfo($destination_path, PATHINFO_EXTENSION));
 
 #Count the rows in the rgstrd_users and increment it by one
 $count_id = mysqli_query($conn, "SELECT COUNT(*) FROM rgstrd_users");
@@ -23,8 +23,10 @@ $id = ++$id;
 
 #Check if the file is an Image
 for ($i = 0; $i < $total; $i++) {
-    if($imageFileType[$i] != "jpg" && $imageFileType[$i] != "png" && $imageFileType[$i] != "jpeg"
-    && $imageFileType[$i] != "gif" ) {
+    if (
+        $imageFileType[$i] != "jpg" && $imageFileType[$i] != "png" && $imageFileType[$i] != "jpeg"
+        && $imageFileType[$i] != "gif"
+    ) {
         #Error for wrong file type
         echo ("<script LANGUAGE='JavaScript'>
         window.alert('Error on reading the selected file! Please check your file if it's an image file.');
@@ -41,26 +43,24 @@ $initiate = mysqli_query($conn, $register);
 #Alert if success or not
 if ($initiate) {
     $dbid = sprintf("%03d.", $id);
-    mkdir($dataset.$dbid.$newfilename, 0777, true);
-    
+    mkdir($dataset . $dbid . $newfilename, 0777, true);
+
     for ($i = '0'; $i < $total; $i++) {
         $tmp_singlepath = $temp_path[$i];
-        $target_path = $dataset.$newfilename.'/'.$dbid.'.'.$newfilename.'_'.$i.'.'.$imageFileType;
+        $target_path = $dataset . $newfilename . '/' . $dbid . '.' . $newfilename . '_' . $i . '.' . $imageFileType;
 
         if (!empty($file_name[$i])) {
             if (move_uploaded_file($tmp_singlepath, $target_path)) {
                 echo ("<script LANGUAGE='JavaScript'>
                     window.alert('Succesfully Registered! Recorded ' . $total . ' images.');
-                    window.location.href='login.php';
+                    window.location.href='index.php';
                     </script>");
             }
         }
     }
-}
-
-else {
+} else {
     echo ("<script LANGUAGE='JavaScript'>
     window.alert('Something went wrong on the server! Please Try again later...');
-    window.location.href='login.php';
+    window.location.href='index.php';
     </script>");
 }
