a08077b2cb6baea6bb0af550b6b1b763c5bc2c3e
Jorge Michael Galang <jorgegalang21@gmail.com>
2022-05-24 11:54:25 +0800

    Fixed delete append entry

--- accept.php
+++ accept.php
@@ -1,49 +1,38 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname = "fr";
+include 'config.php';
 
-$conn = new mysqli($host, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection Failed!");
-} else {
-    echo 'Connected.';
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $location = mysqli_query($conn, "SELECT * FROM `pending_users`");
-    $row = mysqli_fetch_assoc($location);
-    $firstname = $row['ru_firstname'];
-    $lastname = $row['ru_lastname'];
-    $course = $row['ru_course'];
-    $studentid = $row['ru_studentid'];
-    $email = $row['ru_email'];
+$from = mysqli_query($conn, "SELECT * FROM `pending_users`");
+$from_row = mysqli_fetch_assoc($from);
+$pending_id = $from_row['id'];
+$firstname = $from_row['ru_firstname'];
+$lastname = $from_row['ru_lastname'];
+$course = $from_row['ru_course'];
+$studentid = $from_row['ru_studentid'];
+$email = $from_row['ru_email'];
 
 
-    #Counter function
-    $count_id = mysqli_query($conn, "SELECT COUNT(*) FROM rgstrd_users");
-    $count_array = mysqli_fetch_array($count_id);
-    $count = $count_array[0];
+#Counter function
+$count_id = mysqli_query($conn, "SELECT COUNT(*) FROM rgstrd_users");
+$count_array = mysqli_fetch_array($count_id);
+$count = $count_array[0];
 
-    for ($i = 0; $i < $count; $i++) {
-        $registered = mysqli_query($conn, "SELECT * FROM `rgstrd_users` WHERE `id` = $i");
-        $row = mysqli_fetch_row($registered);
-        if (empty($row[1])) {
+for ($i = 0; $i < $count; $i++) {
+    $registered = mysqli_query($conn, "SELECT * FROM `rgstrd_users` WHERE `id` = $i");
+    $row = mysqli_fetch_row($registered);
+    if (empty($row[1])) {
 
-            $destination = mysqli_query($conn, "REPLACE INTO `rgstrd_users` (`id`, `ru_firstname`, `ru_lastname`, `ru_studentid`, `ru_course`, `ru_email`) VALUES ('$i','$firstname','$lastname','$studentid','$course','$email')");
-            if ($destination) {
-                $location = mysqli_query($conn, "DELETE FROM `pending_users` WHERE `id` = $i");
-                echo "DELETED";
+        $destination = mysqli_query($conn, "REPLACE INTO `rgstrd_users` (`id`, `ru_firstname`, `ru_lastname`, `ru_studentid`, `ru_course`, `ru_email`) VALUES ('$i','$firstname','$lastname','$studentid','$course','$email')");
+        if ($destination) {
+            $delete_pending = mysqli_query($conn, "DELETE FROM `pending_users` WHERE `id` = $pending_id");
+            if($delete_pending){
+                echo "<script LANGUAGE='JavaScript'>
+                    window.alert('Accepted '.$firstname.' '.$lastname.'!');
+                    window.location.href='Request.php';
+                    </script>";
             }
+            
         }
     }
 }
+
 ?>
-<html>
-
-<form method="POST">
-    <button href="accept.php?id="> submit</button>
-</form>
-
-</html>
