a4d94498980ac163a5b0bfb5f781d525795584f4
adeldiola <adelainediola@gmail.com>
2022-06-02 00:26:23 +0300

    updated display

--- dashboard.php
+++ dashboard.php
@@ -251,7 +251,7 @@
                <div class="card">
 
                   <div class="card-body text-dark">
-                     <div class="card-header text-white ">QR Code</div>
+                     <div class="card-header text-white ">QR Code Entrance</div>
                      <br>
                      <center>
                         <img src="images/frame.png" width="200px" height="200px">
@@ -263,64 +263,67 @@
                <div class=" card ">
 
                   <div class="card-body text-dark ">
-                     <div class="card-header text-white ">Face Recognition</div>
+                     <div class="card-header text-white ">QR Code Exit</div>
                      <br>
+                     <center>
+                        <img src="images/frame.png" width="200px" height="200px">
 
-                     <h5> Status:</h5>
+                     </center>
+                     <!--    <h5> Status:</h5> -->
 
-                     <?php
+                     <!--            <?php
 
-                     require 'config.php';
-
-
-                     $query = "SELECT id FROM rgstrd_users ORDER BY id"; // To fetch data throough id
-                     $query_run = mysqli_query($conn, $query);
-
-                     $row = mysqli_num_rows($query_run); //Fetch number of row
-
-                     echo '<p>' . $row . '</p>'; // To call rows inside <p>
+                                       require 'config.php';
 
 
-                     ?>
+                                       $query = "SELECT id FROM rgstrd_users ORDER BY id"; // To fetch data throough id
+                                       $query_run = mysqli_query($conn, $query);
+
+                                       $row = mysqli_num_rows($query_run); //Fetch number of row
+
+                                       echo '<p>' . $row . '</p>'; // To call rows inside <p>
+
+
+                                       ?>
                   </div>
                </div>
+            </div> --->
+                  </div>
+               </div>
+
             </div>
-         </div>
-      </div>
 
-      </div>
+            <script>
+               // <!--/. tells about the time  -->
+               function show2() {
+                  if (!document.all && !document.getElementById)
+                     return
+                  thelement = document.getElementById ? document.getElementById("tick2") : document.all.tick2
+                  var Digital = new Date()
+                  var hours = Digital.getHours()
+                  var minutes = Digital.getMinutes()
+                  var seconds = Digital.getSeconds()
+                  var dn = "PM"
+                  if (hours < 12)
+                     dn = "AM"
+                  if (hours > 12)
+                     hours = hours - 12
+                  if (hours == 0)
+                     hours = 12
+                  if (minutes <= 9)
+                     minutes = "0" + minutes
+                  if (seconds <= 9)
+                     seconds = "0" + seconds
+                  var ctime = hours + ":" + minutes + ":" + seconds + " " + dn
+                  thelement.innerHTML = ctime
+                  setTimeout("show2()", 1000)
+               }
+               window.onload = show2
+               //-->
+            </script>
 
-      <script>
-         // <!--/. tells about the time  -->
-         function show2() {
-            if (!document.all && !document.getElementById)
-               return
-            thelement = document.getElementById ? document.getElementById("tick2") : document.all.tick2
-            var Digital = new Date()
-            var hours = Digital.getHours()
-            var minutes = Digital.getMinutes()
-            var seconds = Digital.getSeconds()
-            var dn = "PM"
-            if (hours < 12)
-               dn = "AM"
-            if (hours > 12)
-               hours = hours - 12
-            if (hours == 0)
-               hours = 12
-            if (minutes <= 9)
-               minutes = "0" + minutes
-            if (seconds <= 9)
-               seconds = "0" + seconds
-            var ctime = hours + ":" + minutes + ":" + seconds + " " + dn
-            thelement.innerHTML = ctime
-            setTimeout("show2()", 1000)
-         }
-         window.onload = show2
-         //-->
-      </script>
-
-      <script src="/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
-      <script src="js/bootstrap.min.js"></script>
+            <script src="/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+            <script src="js/bootstrap.min.js"></script>
    </body>
 
    </html>
