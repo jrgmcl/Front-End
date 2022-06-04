c8d3e19b0b5205368c74edda78d30b1cfd8ad9c3
jrgmcl <jorgegalang21@gmail.com>
2022-06-04 20:56:08 +0800

    .

--- test.php
+++ test.php
@@ -8,9 +8,9 @@ $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
 	die("Connection Failed!");
 }
-echo ("<script LANGUAGE='JavaScript'>
-window.location.href='Front-End/index.php';
-window.alert('Error on reading the selected file! Please check your file if it is an ".$host."image file.');
-</script>");
+
+session_start();
+if 
+
 ?>
 
